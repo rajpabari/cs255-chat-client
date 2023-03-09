6d4f0faf0e7d8efd4fe5f34e459ec352e8c90836
Lichu Acuña <lichu@DN51vd7a.SUNet>
2023-03-09 10:12:35 -0800

    fixed 'name' indexing

--- messenger.js
+++ messenger.js
@@ -85,51 +85,48 @@ class MessengerClient {
   async sendMessage(name, plaintext) {
     if (!(name in this.conns)) {
       const RK = await computeDH(this.EGKeyPair.sec, this.certs[name].pub);
-      this.conns.name = { RK: RK, DHr: this.certs[name].pub };
+      this.conns[name] = { RK: RK, DHr: this.certs[name].pub };
 
       //perform first ratchet
       this.EGKeyPair = await generateEG();
       const hkdfOutputRatchet = await HKDF(
-        this.conns.name.RK,
-        await computeDH(this.EGKeyPair.sec, this.conns.name.DHr),
+        this.conns[name].RK,
+        await computeDH(this.EGKeyPair.sec, this.conns[name].DHr),
         "ratchet-str"
       );
-      this.conns.name.RK = hkdfOutputRatchet[0];
-      this.conns.name.CKs = hkdfOutputRatchet[1];
-      this.conns.name.CKr = "";
-
+      this.conns[name].RK = hkdfOutputRatchet[0];
+      this.conns[name].CKs = hkdfOutputRatchet[1];
+      this.conns[name].CKr = "";
     }
 
-    const messageKey = await HMACtoAESKey(this.conns.name.CKs, govEncryptionDataStr);
-    this.conns.name.CKs = await HMACtoHMACKey(this.conns.name.CKs, "HMACKeyGen");
+    const messageKey = await HMACtoAESKey(this.conns[name].CKs, govEncryptionDataStr);
+    this.conns[name].CKs = await HMACtoHMACKey(this.conns[name].CKs, "HMACKeyGen");
 
     const iv = genRandomSalt();
     const ciphertext = await encryptWithGCM(messageKey, plaintext, iv);
 
-    const header = { receiverIV: iv, pub: this.EGKeyPair.pub, "vGov": "", "cGov": "", "ivGov": "" }
+    const header = { receiverIV: iv, pub: this.EGKeyPair.pub, vGov: "", cGov: "", ivGov: "" };
     return [header, ciphertext];
   }
 
-  async DHRatchet(header) {
-
-    this.conns.name.DHr = header.pub
+  async DHRatchet(name, header) {
+    this.conns[name].DHr = header.pub;
     const hkdfOutputRatchet1 = await HKDF(
-      this.conns.name.RK,
-      await computeDH(this.EGKeyPair.sec, this.conns.name.DHr),
+      this.conns[name].RK,
+      await computeDH(this.EGKeyPair.sec, this.conns[name].DHr),
       "ratchet-str"
     );
-    this.conns.name.RK = hkdfOutputRatchet1[0];
-    this.conns.name.CKr = hkdfOutputRatchet1[1];
-
+    this.conns[name].RK = hkdfOutputRatchet1[0];
+    this.conns[name].CKr = hkdfOutputRatchet1[1];
 
     this.EGKeyPair = await generateEG();
     const hkdfOutputRatchet2 = await HKDF(
-      this.conns.name.RK,
-      await computeDH(this.EGKeyPair.sec, this.conns.name.DHr),
+      this.conns[name].RK,
+      await computeDH(this.EGKeyPair.sec, this.conns[name].DHr),
       "ratchet-str"
     );
-    this.conns.name.RK = hkdfOutputRatchet2[0];
-    this.conns.name.CKs = hkdfOutputRatchet2[1];
+    this.conns[name].RK = hkdfOutputRatchet2[0];
+    this.conns[name].CKs = hkdfOutputRatchet2[1];
   }
 
   /**
@@ -142,19 +139,18 @@ class MessengerClient {
    * Return Type: string
    */
   async receiveMessage(name, [header, ciphertext]) {
-    // compare the public key header with the public key in the connection DHr
-    // if they are not the same, then we need to recompute the ratchet (use flag )
     if (!(name in this.conns)) {
       const RK = await computeDH(this.EGKeyPair.sec, this.certs[name].pub);
-      this.conns.name = { RK: RK, DHr: "" };
+      // DHr will be updated when calling DHRatchet()
+      this.conns[name] = { RK: RK };
     }
 
-    if (header.pub !== this.conns.name.DHr) {
-      await this.DHRatchet(header);
+    if (header.pub !== this.conns[name].DHr) {
+      await this.DHRatchet(name, header);
     }
 
-    const messageKey = await HMACtoAESKey(this.conns.name.CKs, govEncryptionDataStr);
-    this.conns.name.CKs = await HMACtoHMACKey(this.conns.name.CKs, "HMACKeyGen");
+    const messageKey = await HMACtoAESKey(this.conns[name].CKs, govEncryptionDataStr);
+    this.conns[name].CKs = await HMACtoHMACKey(this.conns[name].CKs, "HMACKeyGen");
 
     //the following line is causing the cipher job failure
     const plaintext = await decryptWithGCM(messageKey, ciphertext, header.receiverIV);
